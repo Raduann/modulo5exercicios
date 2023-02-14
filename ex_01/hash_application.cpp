0b3dab38e51b1e0dd7ec4419b9ad6cccd8a849b8
Raduann <raduan.muarrek@sou.inteli.edu.br>
2023-02-14 17:37:58 -0300

    Update hash_application.cpp

--- ex_01/hash_application.cpp
+++ ex_01/hash_application.cpp
@@ -11,21 +11,21 @@ int main(){
   string user[5] = {
     "sasuke.uchiha@sou.inteli.edu.br", "kakashi.hatake@sou.inteli.edu.br", "sakura.haruno@sou.mtofraca.edu.br", "rock.lee@sou.top.edu.br", "danzou.shimura@sou.cuzao.edu.br"}; 
 
- for (int i = 0; i < 5; i++) {
-    Cliente cliente = cliente(ras[i], nomes[i]);
-    clienteHash.insertItem(cliente);
+ for (int i = 0; i < 7; i++) {
+    Cliente cliente = Cliente(imei[i], user[i]);
+    clientesHash.insertItem(cliente);
   }
-  clienteHash.print();  
+  clientesHash.print();  
   cout << "------------------------------" <<  endl;
   
   Cliente cliente(16489,"");  
   bool     found = false;
-  clienteHash.retrieveItem(cliente, found);
-  cout << cliente.getNome() << " -> " << found << endl;
+  clientesHash.retrieveItem(cliente, found);
+  cout << cliente.getUser() << " -> " << found << endl;
   
   cout << "------------------------------" <<  endl;
 
-  clienteHash.deleteItem(cliente);
-  clienteHash.print();
+  clientesHash.deleteItem(cliente);
+  clientesHash.print();
   cout << "Fim" << endl;  
 }
